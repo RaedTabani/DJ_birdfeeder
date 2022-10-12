ad7c7275b9f49463b760631deacc150f6dba69f5
Raed Tabani <raedtabanics@gmail.com>
2022-10-12 10:10:18 +0200

    Update BuildingUI.cs
    #Test

--- Assets/Birdfeeder/Scripts/Game/UI/BuildingUI.cs
+++ Assets/Birdfeeder/Scripts/Game/UI/BuildingUI.cs
@@ -24,6 +24,7 @@ public class BuildingUI : MonoBehaviour
         buy.interactable = false;
         cost.text = "Unlock at "+ model.cost;
         status.enabled =true;
+        Debug.Log("");
     }
 
 }
